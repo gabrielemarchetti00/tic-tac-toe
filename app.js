48a730f893b21d4117d3eb3ced8368feda5e19e9
gabrielemarchetti00 <gabriele.marchetti26@gmail.com>
2023-03-25 22:46:56 +0100

    add basic structure, to fix bugs

--- app.js
+++ app.js
@@ -1,27 +1,66 @@
 const gameBoard = (() => {
-    let gameboard = ['', 'x', 'o', 'x', 'o', '', '', 'x', ''];
-    return {gameboard};
+    let board = ['', 'x', 'o', 'x', 'o', '', '', 'x', ''];
+    const getBoard = () => board;
+
+    const placeSymbol = (cell, player) => {
+        board[cell] = player.symbol;
+    };
+
+    return {getBoard, placeSymbol};
 })();
 
-const displayController = (() => {
-
-})();
-
-const player = () => {
-
+const player = (name, symbol) => {
+    return {name, symbol};
 };
 
-function render(){
-    let i = 0;
+const gameController = (() => {
+    const board = gameBoard.getBoard();
+    const playerOne = player('Player one', 'x');
+    const playerTwo = player('Player two', 'o')
+
+    let activePlayer = playerOne;
+    const getActivePlayer = () => activePlayer;
+
+    const switchPlayerTurn = () => {
+        activePlayer = activePlayer === playerOne ? playerTwo : playerOne;
+    };
+
+    const playRound = (cell) => {
+        gameBoard.placeSymbol(cell, getActivePlayer());
+    };
+
+    switchPlayerTurn();
+
+    return {playRound, getActivePlayer, getBoard: board.getBoard};
+})();
+
+
+const displayController = (() => {
+    const playerTurnDiv = document.querySelector('.turn');
     let tds = document.querySelectorAll('td');
-    tds.forEach((td) => {
-        for(i in gameBoard.gameboard){
-            if(i == td.id){
-                td.textContent = gameBoard.gameboard[i];
+
+    const updateScreen = () => {
+        const activePlayer = gameController.getActivePlayer();
+
+        playerTurnDiv.textContent = `${activePlayer.name}'s turn...`;
+
+        let i = 0;
+        tds.forEach((td) => {
+            for(i in gameBoard.getBoard()){
+                if(i == td.id){
+                    td.textContent = gameBoard.getBoard()[i];
+                }
             }
-        }
+        });
+    }
+
+    function clickHandlerBoard(id){
+        gameController.playRound(id);
+        updateScreen();
+    };
+    tds.forEach((td) => {
+        td.addEventListener('click', clickHandlerBoard(td.id));
     });
-}
 
-render();
-
+    updateScreen();
+})();
